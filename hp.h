0611512899f6d3dc2118df1b8a1f0f07ed06f039
meet <meet@undefined.hostname.localhost>
2025-06-11 17:21:01 +0530

    Core structure

---
+++ hp.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "hitpointtype.h"
+
+class hp {
+	public:
+		// returns true is setting is successful
+		bool setMaxHP(hptype new_max_hp){
+			if (new_max_hp < 1){
+				return false;
+			}
+			MaxHP = new_max_hp;
+
+			if (CurrentHP > MaxHP){
+				CurrentHP = MaxHP;
+			}
+
+			return true;
+		}
+
+		hptype getMaxHP(){
+			return MaxHP;
+		}
+
+		hptype getCurrentHP(){
+			return CurrentHP;
+		}
+
+		void takeDamage(hptype damage){
+			if (damage > CurrentHP + ShieldHP){
+				CurrentHP = 0;
+				return;
+			}
+			
+			CurrentHP -= damage;
+		}
+		
+		void heal(hptype amount) {
+			if (amount + CurrentHP > MaxHP){
+				CurrentHP = MaxHP;
+				return;
+			}
+				CurrentHP += amount;
+		}
+
+
+
+	private:
+		hptype CurrentHP;
+		hptype MaxHP;
+};	
